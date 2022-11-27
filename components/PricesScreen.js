2cec33ed0ee8d5ee73a8b28c87161beeaf1d7d4c
Pietari Tanner <cotapi01@students.oamk.fi>
2022-11-27 20:52:40 +0200

    Hinta sivulle lisätty background task ja notivication. Molemmat vielä testaamatta kunnolla.

--- components/PricesScreen.js
+++ components/PricesScreen.js
@@ -11,17 +11,14 @@ import {
 } from "react-native-chart-kit";
 import { Col, Grid } from 'react-native-easy-grid'
 import moment from 'moment';
-import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'react-native-simple-radio-button';
 import styles from '../styles/styles';
 import { useFonts } from 'expo-font';
 import { useRef } from 'react';
 import DropDownPicker from 'react-native-dropdown-picker'
-
-
-
-
-
-
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import * as Notifications from "expo-notifications";
 
 const URL = 'https://web-api.tp.entsoe.eu/api?'
 const TOKEN = 'securityToken=419b446b-122c-414f-8586-fc7d6ff39def'
@@ -29,6 +26,27 @@ const DOCTYPE = '&documentType=A44'
 const OUT_BIDD_ZONE = '&outBiddingZone_Domain=10YFI-1--------U'
 const IN_DOM = '&in_Domain=10YFI-1--------U'
 const OUT_DOM = '&out_Domain=10YFI-1--------U'
+
+// BackGroundtask
+let setStateFn = () => {
+    console.log("State not yet initialized");
+};
+
+// notification
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+    }),
+});
+
+export async function allowsNotificationsAsync() {
+    const settings = await Notifications.getPermissionsAsync();
+    return (
+        settings.granted || settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+    );
+}
 
 const PricesScreen = () => {
     const [answer, setAnswer] = useState();
@@ -44,30 +62,58 @@ const PricesScreen = () => {
         { label: 'Edelliset 31 vrk', value: 'LastMonth' },
     ]);
 
-    useEffect(() => {
-        //let interval = setInterval(() => {
-        // Get date, the previous seven days and the previous 31 days
-        let curDate = moment().utcOffset('+02:00').format('YYYYMMDDHH00');
-        let dayAhead = moment().add(1, 'd').format('YYYYMMDDHH00')
-        let lastMonth = moment().subtract(31, 'd').format('YYYYMMDDHH00')
-        setHour(curDate.substring(8, 10))
+    const [state, setState] = useState(null);
 
-        fetch(URL + TOKEN + DOCTYPE + OUT_BIDD_ZONE + '&periodStart=' + lastMonth + '&periodEnd=' + dayAhead + IN_DOM + OUT_DOM)
-            .then(resp => resp.text())
-            .then(data => {
-                let XMLParser = require('react-xml-parser');
-                const xml = new XMLParser().parseFromString(data);
-                setAnswer(xml.getElementsByTagName('Price'))
-                setAllData(xml.getElementsByTagName('Period'))
-            })
-            .catch(e => console.log(e))
-        /* 
-                }, 50000);
-        
-                return () => {
-                    clearInterval(interval)
+    // Background Task
+    setStateFn = setState;
+    const getPrice = 'getPriceData'
+    async function getPriceData() {
+        async function initBackgroundFetch(GetPriceData,
+            taskFn,
+            interval = 60 * 1,) {
+            try {
+                if (!TaskManager.isTaskDefined(GetPriceData)) {
+                    TaskManager.defineTask(GetPriceData, taskFn);
                 }
-         */
+                const options = {
+                    minimumInterval: interval = 60 * 1// Minute
+                };
+                await BackgroundFetch.registerTaskAsync(GetPriceData, options);
+            } catch (err) {
+                return { err }
+            }
+        }
+        try {
+            //let interval = setInterval(() => {
+            // Get date, the previous seven days and the previous 31 days
+            let curDate = moment().utcOffset('+02:00').format('YYYYMMDDHH00');
+            let dayAhead = moment().add(1, 'd').format('YYYYMMDDHH00')
+            let lastMonth = moment().subtract(31, 'd').format('YYYYMMDDHH00')
+            setHour(curDate.substring(8, 10))
+            await fetch(URL + TOKEN + DOCTYPE + OUT_BIDD_ZONE + '&periodStart=' + lastMonth + '&periodEnd=' + dayAhead + IN_DOM + OUT_DOM)
+                .then(resp => resp.text())
+                .then(data => {
+                    let XMLParser = require('react-xml-parser');
+                    const xml = new XMLParser().parseFromString(data);
+                    setAnswer(xml.getElementsByTagName('Price'))
+                    setAllData(xml.getElementsByTagName('Period'))
+                })
+                .catch(e => console.log(e))
+            return backendData
+                ? BackgroundFetch.Result.NewData
+                : BackgroundFetch.Result.NoData;
+        } catch (err) {
+        }
+
+        initBackgroundFetch(getPrice, getPriceData, 5);
+    }
+
+
+
+
+    useEffect(() => {
+        getPriceData()
+        schedulePushNotification()
     }, [])
 
 
@@ -80,7 +126,7 @@ const PricesScreen = () => {
 
     // Alv arvot
     const alvData = [
-        { label: 'Alv 24%', value: 24},
+        { label: 'Alv 24%', value: 24 },
         { label: 'Alv 10%', value: 10 },
         { label: 'Alv 0%', value: 1 }];
 
@@ -142,28 +188,40 @@ const PricesScreen = () => {
     // Datat charteille
 
     // Päivän/viikon/kk alin/avg/ylin...
-   let dataDay = ''
-   if (value === 'Today' || value === null) {
-       dataDay = {
-           labels: ["ALIN","AVG", "YLIN"], 
-           datasets: [{data: [((100 + alv) / 100 * todayMin * 0.1).toFixed(2),
-                   ((100 + alv) / 100 * avgToday).toFixed(2),
-                   ((100 + alv) / 100 * todayMax * 0.1).toFixed(2)]}]}}
-   if (value === 'LastSeven') {
-       dataDay = {
-           labels: ["ALIN","AVG", "YLIN"], 
-           datasets: [{data: [((100 + alv) / 100 * weekMin * 0.1).toFixed(2),
-               ((100 + alv) / 100 * avgLastWeek * 0.1).toFixed(2),
-               ((100 + alv) / 100 * weekMax * 0.1).toFixed(2)]}]}}
-   if (value === 'LastMonth') {
-       dataDay = {
-           labels: ["ALIN","AVG", "YLIN"], 
-           datasets: [{data: [((100 + alv) / 100 * monthMin * 0.1).toFixed(2),
-                   ((100 + alv) / 100 * avgMonth * 0.1).toFixed(2),
-                   ((100 + alv) / 100 * monthMax * 0.1).toFixed(2)]}]}}
+    let dataDay = ''
+    if (value === 'Today' || value === null) {
+        dataDay = {
+            labels: ["ALIN", "AVG", "YLIN"],
+            datasets: [{
+                data: [((100 + alv) / 100 * todayMin * 0.1).toFixed(2),
+                ((100 + alv) / 100 * avgToday).toFixed(2),
+                ((100 + alv) / 100 * todayMax * 0.1).toFixed(2)]
+            }]
+        }
+    }
+    if (value === 'LastSeven') {
+        dataDay = {
+            labels: ["ALIN", "AVG", "YLIN"],
+            datasets: [{
+                data: [((100 + alv) / 100 * weekMin * 0.1).toFixed(2),
+                ((100 + alv) / 100 * avgLastWeek * 0.1).toFixed(2),
+                ((100 + alv) / 100 * weekMax * 0.1).toFixed(2)]
+            }]
+        }
+    }
+    if (value === 'LastMonth') {
+        dataDay = {
+            labels: ["ALIN", "AVG", "YLIN"],
+            datasets: [{
+                data: [((100 + alv) / 100 * monthMin * 0.1).toFixed(2),
+                ((100 + alv) / 100 * avgMonth * 0.1).toFixed(2),
+                ((100 + alv) / 100 * monthMax * 0.1).toFixed(2)]
+            }]
+        }
+    }
 
-   
-  
+
+
     // Charttien kokoonpano
     const chartConfig = {
         backgroundGradientFrom: "#1f2131",
@@ -174,109 +232,137 @@ const PricesScreen = () => {
         strokeWidth: 2, // optional, default 3
         barPercentage: 0.5,
         useShadowColorFromDataset: false, // optional 
-        
+
     };
 
-    
+    // Notification 
+    async function schedulePushNotification() {
 
+        const hasPushNotificationPermissionGranted = await allowsNotificationsAsync()
+        console.log(hasPushNotificationPermissionGranted)
+        if (hasPushNotificationPermissionGranted && ((100 + alv) / 100 * sum * 0.1).toFixed(2) > 30) {
+            await Notifications.scheduleNotificationAsync({
+                content: {
+                    title: "Hinta tieto",
+                    body: 'Pörssisähkön hinta on nyt kallista.',
+                    data: { data: 'goes here' },
+                },
+                trigger: { seconds: 2 },
+            });
+
+        }
+        if (hasPushNotificationPermissionGranted && ((100 + alv) / 100 * sum * 0.1).toFixed(2) < 10) {
+            await Notifications.scheduleNotificationAsync({
+                content: {
+                    title: "Hinta tieto",
+                    body: 'Pörssisähkön hinta on nyt halpaa.',
+                    data: { data: 'goes here' },
+                },
+                trigger: { seconds: 2 },
+            });
+
+        }
+    }
+
+    console.log(((100 + alv) / 100 * sum * 0.1).toFixed(2))
     return (
         <View style={[styles.home]} >
-            
-           
-                
-              
-                <View style={[{marginBottom: 20, marginTop: 20}]}>
-                    <RadioForm
-                        //style={theme.radio}
-                        buttonSize={10}
-                        buttonOuterSize={20}
-                        radio_props={alvData}
-                        initial={0}
-                        onPress={(value) => { setAlv(value) }}
-                        buttonColor={'#a6d3d8'}
-                        selectedButtonColor={'#b64600'}
-                        labelColor={'#a6d3d8'}
-                        selectedLabelColor={'#a6d3d8'}
-                        formHorizontal={true}
-                        labelHorizontal={false}
-                        
-                    />
-                </View> 
-                <View>
-                    <DropDownPicker
-                        
-                        theme="DARK"
-                        placeholder='Tämä päivä'
-                        open={open}
-                        value={value}
-                        items={items}
-                        setOpen={setOpen}
-                        setValue={setValue}
-                        setItems={setItems}
-                        //defaultIndex={0}
-                        containerStyle={{ height: 30 }}
-                        onChangeItem={item => setValue(item.value)}
-                    />
-                </View>
+
+
+
+
+            <View style={[{ marginBottom: 20, marginTop: 20 }]}>
+                <RadioForm
+                    //style={theme.radio}
+                    buttonSize={10}
+                    buttonOuterSize={20}
+                    radio_props={alvData}
+                    initial={0}
+                    onPress={(value) => { setAlv(value) }}
+                    buttonColor={'#a6d3d8'}
+                    selectedButtonColor={'#b64600'}
+                    labelColor={'#a6d3d8'}
+                    selectedLabelColor={'#a6d3d8'}
+                    formHorizontal={true}
+                    labelHorizontal={false}
+
+                />
+            </View>
+            <View>
+                <DropDownPicker
+
+                    theme="DARK"
+                    placeholder='Tämä päivä'
+                    open={open}
+                    value={value}
+                    items={items}
+                    setOpen={setOpen}
+                    setValue={setValue}
+                    setItems={setItems}
+                    //defaultIndex={0}
+                    containerStyle={{ height: 30 }}
+                    onChangeItem={item => setValue(item.value)}
+                />
+            </View>
             <ScrollView>
                 <View width={Dimensions.get("window").width} >
                     <Grid >
-                        <Col style={{ alignItems: 'center', marginTop: 75}}>
+                        <Col style={{ alignItems: 'center', marginTop: 75 }}>
                             <BarChart
-                            //style={graphStyle}
-                            data={dataDay}
-                            width={300}
-                            height={250}
-                            yAxisSuffix=" snt"
-                            chartConfig={chartConfig}
-                            verticalLabelRotation={30}
-                            fromZero={true}
-                            showValuesOnTopOfBars={true}
-                            showBarTops={false}
+                                //style={graphStyle}
+                                data={dataDay}
+                                width={300}
+                                height={250}
+                                yAxisSuffix=" snt"
+                                chartConfig={chartConfig}
+                                verticalLabelRotation={30}
+                                fromZero={true}
+                                showValuesOnTopOfBars={true}
+                                showBarTops={false}
 
-                            bezier
-                            style={{
-                                marginVertical: 8,
-                                borderRadius: 5,
-                                shadowColor: "#a6d3d8",
-                                elevation: 5,
-                                
-                            }}
-                            
-                        />
-                        
+                                bezier
+                                style={{
+                                    marginVertical: 8,
+                                    borderRadius: 5,
+                                    shadowColor: "#a6d3d8",
+                                    elevation: 5,
+
+                                }}
+
+                            />
+
                         </Col>
                     </Grid>
                 </View>
 
-                
+
                 <View style={styles.home} width={Dimensions.get("window").width}>
                     <Col style={{ alignItems: 'center', marginTop: 50, marginBottom: 30 }}>
-                        <Text style={[styles.text,{marginBottom: 20, fontSize: 20}]}>Hinta nyt</Text>
+                        <Text style={[styles.text, { marginBottom: 20, fontSize: 20 }]}>Hinta nyt</Text>
                         <View style={{
-                                        width: 150,
-                                        height: 150,
-                                        backgroundColor: '#0000',
-                                        borderRadius: 10,
-                                        paddingHorizontal: 40,
-                                        
-                                        
-                                        alignSelf: 'center',
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                        shadowColor: "#a6d3d8",
-                                        shadowOffset: {width: 0, height: 0},
-                                        shadowOpacity: 0.8,
-                                        elevation: 4
-                                    }}>
+                            width: 150,
+                            height: 150,
+                            backgroundColor: '#0000',
+                            borderRadius: 10,
+                            paddingHorizontal: 40,
+
+
+                            alignSelf: 'center',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            shadowColor: "#a6d3d8",
+                            shadowOffset: { width: 0, height: 0 },
+                            shadowOpacity: 0.8,
+                            elevation: 4
+                        }}>
                             <Text style={styles.text}>{((100 + alv) / 100 * sum * 0.1).toFixed(2)} snt/kWh</Text>
                         </View>
                     </Col>
                 </View>
-                
+
                 <View >
                     <Col style={{ alignItems: 'center', marginTop: 30 }}>
-                        <Text style={[styles.text,{margin: 20, fontSize: 16}]}>Hinta tänään</Text>
+                        <Text style={[styles.text, { margin: 20, fontSize: 16 }]}>Hinta tänään</Text>
                         <LineChart
                             data={{
                                 labels: ["01:00", "03:00", "05:00", "07:00", "10:00",
@@ -292,7 +378,7 @@ const PricesScreen = () => {
                             yAxisInterval={1} // optional, defaults to 1
                             fromZero={true}
                             chartConfig={{
-                                
+
                                 backgroundColor: "#0000",
                                 backgroundGradientFrom: "#1f2131",
                                 backgroundGradientTo: "#1f2131",
@@ -314,14 +400,14 @@ const PricesScreen = () => {
                                 borderRadius: 16,
                                 shadowColor: "#a6d3d8",
                                 elevation: 10,
-                                
+
                             }}
                         />
                     </Col>
                 </View>
                 <View width={Dimensions.get("window").width}>
                     <Col style={{ alignItems: 'center', marginTop: 30 }} >
-                        <Text style={[styles.text,{marginBottom: 20, fontSize: 16}]}>Hinta huomenna (julkaistaan päivittäin kello 14:00)</Text>
+                        <Text style={[styles.text, { marginBottom: 20, fontSize: 16 }]}>Hinta huomenna (julkaistaan päivittäin kello 14:00)</Text>
                         <LineChart
                             data={{
                                 labels: ["01:00", "03:00", "05:00", "07:00", "10:00",
@@ -360,7 +446,7 @@ const PricesScreen = () => {
                                 borderRadius: 16,
                                 shadowColor: "#a6d3d8",
                                 elevation: 10
-                                
+
                             }}
                         />
                     </Col>
