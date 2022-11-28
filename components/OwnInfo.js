18d798818d69457f2f08088b9c218d72500f48df
xkangk <n0jaka00@students.oamk.fi>
2022-11-28 15:37:17 +0200

    Logot

---
+++ components/OwnInfo.js
@@ -0,0 +1,10 @@
+import { View,Text } from "react-native";
+import styles from '../styles/styles';
+const OwnInfo = () => {
+    return(
+        <View style={styles.home}>
+            
+        </View>
+    );
+}
+export {OwnInfo}
