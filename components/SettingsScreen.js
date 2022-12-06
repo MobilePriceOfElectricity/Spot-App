526e2c3fe2e1777d20efa02ea2a41b7befd54d63
xkangk <n0jaka00@students.oamk.fi>
2022-12-06 23:46:01 +0200

    SplashScreen päivitetty, Settings sivut lisätty

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,12 +1,44 @@
-import { View } from "react-native";
+import { View,Switch,Text } from "react-native";
 import styles from '../styles/styles';
+import React, { useState } from "react";
+import Slider from '@react-native-community/slider';
 
 const SettingsScreen = () => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  
     return (
-        <View style={styles.home}>
-
-
-        </View>
+      <View style={styles.container}>
+        <View>
+            <Text style={styles.text}>Salli push ilmoitukset</Text>
+            <Switch
+        
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      </View>
+      <View><Text style={styles.text}>Hintaraja</Text><Slider
+  style={{width: 200, height: 40,}}
+  minimumValue={0}
+  maximumValue={1}
+  minimumTrackTintColor="#FFFFFF"
+  maximumTrackTintColor="#FFFFFF"
+/></View>
+<View>
+  <Text style={styles.text}>Tietoja</Text>
+  <Text style={styles.text}>Datan lähteet: </Text>
+      <Text style={styles.text}>Fingrid & ENTSO-E:</Text>
+      <Text style={styles.text}>Tietosuoja:</Text>
+      <Text style={styles.text}>Sovellus ei kerää käyttäjästä mitään henkilökohtaista dataa, ainoa kerättävä data on käyttäjän syöttämät kodinkoneet ja niiden kwh arvot.</Text>
+      <Text style={styles.text}>Käyttöehdot:</Text>
+      <Text style={styles.text}>Sovelluksen tekijät eivät vastaa hintatietojen oikeellisuudesta, 
+        sillä data saadaan kolmannelta osapuolelta. Jatkuvaa toimivuutta ei tämän takia voida sovellukselle taata. 
+        Käyttäjä vastaa itse syöttämistään arvoista ja niissä olevista virheistä ei sovelluksen kehittäjät vastaa. Laskukaavat antavat joka tapauksessa viitteellisen arvion.  </Text></View>
+        
+      </View>
         
     );
 } 
