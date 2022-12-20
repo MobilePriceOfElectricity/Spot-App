33cd634fe78e442128355e539dd000c12d7cd3f5
xkangk <n0jaka00@students.oamk.fi>
2022-12-20 19:27:50 +0200

    Omat kodinkoneet tehty viimeistelyt

--- components/InputModal.js
+++ components/InputModal.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Modal } from "react-native";
+import React, {useState, useEffect} from "react";
+import { Alert, Modal } from "react-native";
 import {
     ModalButton,
     ModalContainer,
@@ -11,7 +11,7 @@ import {
     colors
 } from "../styles/appStyles"
 import {AntDesign} from "@expo/vector-icons"
-
+import moment from 'moment';
 const InputModal = ({modalVisible,
     setModalVisible,
     applianceInputValue,
@@ -23,6 +23,41 @@ const InputModal = ({modalVisible,
     setApplianceToBeEdited,
     handleEditAppliance,
 appliances}) => {
+    const [hourPrice, setHourPrice] = useState([])
+    //liittyy fetchiin
+    const [hour, setHour] = useState();
+
+//Fetch price
+const fetchToday = () => {
+    fetch('http://www.students.oamk.fi/~n0juro00/MobiiliProjekti/GetEstoeeData.php', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            'mode': 'no-cors'
+        }, body: JSON.stringify({
+            key: 'test',
+        })
+    })
+        .then((resp) => resp.json())
+        .then((res) => {
+            setHourPrice(res[0]['24h'])
+        })
+}
+useEffect(() => {
+    fetchToday()
+
+    //liittyy fetchiin
+    let curDate = moment().utcOffset('+02:00').format('YYYYMMDDHH00');
+    setHour(curDate.substring(8, 10))
+}, [])
+
+//Check time
+let priceNow = ''
+for (let i = 0; i < hourPrice.length; i++)
+    if (hour === hourPrice[i].time) {
+        priceNow = ((100 + 10) / 100 * hourPrice[i].price).toFixed(2)
+    }
     const handleCloseModal = () => {
         setModalVisible(false);
         setApplianceInputValue("");
@@ -31,21 +66,46 @@ appliances}) => {
     }
     const handleSubmit = () => {
         if(!applianceToBeEdited){
+            //Tarkistetaan, ettei kentät ole tyhjiä
+            if(applianceInputValue === "" || kwhValue === ""){
+                return Alert.alert(
+                    //Otsikko
+                      'HUOM!',
+                      'Jokin arvo jäi täyttämättä',
+                      [
+                        {
+                            text: '',
+                            onPress: () => console.log('No Pressed'),
+                            style: 'cancel',
+                          },
+                        ,
+                        {
+                          text: 'Sulje',
+                          onPress: () => console.log('No Pressed'),
+                          style: 'cancel',
+                        },
+                      ],
+                      { cancelable: false }
+                      //clicking out side of alert will not cancel
+                        );
+                    }else {
         handleAddAppliance({
             title: applianceInputValue,
             kwh: kwhValue,
+            summa: (priceNow * kwhValue / 100).toFixed(2),
             key:`${(appliances[appliances.length-1] && parseInt(appliances[appliances.length-1].key)+1) || 1}`
-        });
-    }else {
+        })}}
+     else {
         handleEditAppliance({
             title: applianceInputValue,
             kwh: kwhValue,
+            summa: (priceNow * kwhValue / 100).toFixed(2),
             key: applianceToBeEdited.key
         })
     }
         setApplianceInputValue("");
         setKwhValue("");
-    }
+    } 
     return (
         <>
             <ModalButton onPress={()=>{setModalVisible(true)}}>
@@ -74,7 +134,7 @@ appliances}) => {
                         
                     />
                     <StyledInput 
-                        placeholder="kWh"
+                        placeholder="Ilmoita kodinkoneen kuluttama kwh/tunti"
                         placeholderTextColor ={colors.alternative}
                         selectionColor = {colors.secondary}
                         autoFocus={true}
